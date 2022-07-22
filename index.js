4555b30c070f0c20f8642661a2e46f41c98a12c2
Alicia Myrie <aliciamyrie@gmail.com>
2022-07-22 15:28:43 -0400

    second edit

--- index.js
+++ index.js
@@ -8,11 +8,68 @@ initializeApp({
 
 const db = getFirestore();
 
-const car = {
-  make: "Lexus",
-  model: "IS 250",
-  year: "2015",
-  color: "grey",
-};
+// const car = {
+//   make: "Ferrari",
+//   model: "GTO",
+//   year: "2008",
+//   color: "red",
+// };
 
-db.collection("cars").add(car);
+// to add to an object that already exists in a collection
+
+// db.collection("cars")
+//   .add(car)
+//   .then((doc) => {
+//     console.log("Doc added:", doc.id);
+//   })
+//   .catch((err) => console.error(err));
+
+// to edit a doc
+
+// db.collection("cars").doc("lambo").set({
+//   make: "Lamborghini",
+//   model: "Diablo",
+//   year: "2008",
+//   color: "yellow",
+// });
+
+// to update
+
+// db.collection("cars").doc("lambo").update({
+//   model: "Diablo",
+//   color: "hot pink",
+// });
+
+//
+
+db.collection("cars")
+  .doc("lambo")
+  .get()
+  .then((doc) => {
+    console.log(doc.id);
+    console.log(doc.data());
+  })
+  .catch(console.error);
+
+// Get a whole collection:
+
+db.collection("cars")
+  .get()
+  .then((collection) => {
+    collection.docs.forEach((doc) => console.log(doc.data()));
+  })
+  .catch(console.error);
+
+// Query docs from collection:
+db.collection("cars")
+  .where("year", "==", "2008")
+  .get()
+  .then((collection) => {
+    const cars = collection.docs.map((doc) => {
+      let car = doc.data(); // { make, model, color, year }
+      car.id = doc.id; // { make, model, color, year, id }
+      return car;
+    });
+    console.log(cars);
+  })
+  .catch(console.error);
